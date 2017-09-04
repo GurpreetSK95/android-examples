d399e99adc33bcc3ea7c064a735b36e91bf42d74
Gurpreet Singh <gurpreetsk@hotmail.com>
2017-09-04 19:21:25 +0530

    feat: add UsingSimpleSqlProvider project

--- UsingSimpleSqlProvider/app/src/main/java/me/gurpreetsk/usingsimplesqlprovider/MainActivity.java
+++ UsingSimpleSqlProvider/app/src/main/java/me/gurpreetsk/usingsimplesqlprovider/MainActivity.java
@@ -1,13 +1,51 @@
 package me.gurpreetsk.usingsimplesqlprovider;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.List;
+
+import me.gurpreetsk.usingsimplesqlprovider.model.User;
+import me.gurpreetsk.usingsimplesqlprovider.model.UserTable;
+
+
+//https://github.com/ckurtm/simple-sql-provider
 
 public class MainActivity extends AppCompatActivity {
+
+  TextView textView;
+  StringBuilder string = new StringBuilder();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    textView = (TextView) findViewById(R.id.textview);
+
+    insertInDb();
+    getData();
+
+  }
+
+  //query data using content resolver
+  private void getData() {
+    Cursor cursor = getContentResolver().query(UserTable.CONTENT_URI, null, null, null, null);
+    List<User> list = UserTable.getRows(cursor, true);
+    for (User user : list) {
+      string.append(user.getName()).append("\n");
+    }
+    textView.setText(string.toString());
+  }
+
+  //Simple method to insert using content resolver
+  private void insertInDb() {
+    User user = new User("GurpreetSK95", 0);
+    User user1 = new User("Nisrulz", 1);
+    User user2 = new User("Snap", 2);
+    getContentResolver().insert(UserTable.CONTENT_URI, UserTable.getContentValues(user, false));
+    getContentResolver().insert(UserTable.CONTENT_URI, UserTable.getContentValues(user1, false));
+    getContentResolver().insert(UserTable.CONTENT_URI, UserTable.getContentValues(user2, false));
   }
 }
---
+++ UsingSimpleSqlProvider/app/src/main/java/me/gurpreetsk/usingsimplesqlprovider/model/User.java
@@ -0,0 +1,30 @@
+package me.gurpreetsk.usingsimplesqlprovider.model;
+
+import ckm.simple.sql_provider.annotation.SimpleSQLColumn;
+import ckm.simple.sql_provider.annotation.SimpleSQLTable;
+
+/**
+ * Created by Gurpreet on 04/09/17.
+ */
+
+@SimpleSQLTable(table = "user", provider = "MyProvider")
+public class User {
+
+    @SimpleSQLColumn("name")
+    private String name;
+    @SimpleSQLColumn("_id")
+    private int id;
+
+    public User(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
